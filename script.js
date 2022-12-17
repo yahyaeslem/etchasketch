c2172a95237b458477ecd9e79a9085bdfbdd0337
yahyaeslem <yahyaeslem@gmail.com>
2022-12-17 22:38:54 +0300

    16*16 square created

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 
+
+const btnBlack =document.querySelector('.btnBlack');
+const btnErase =document.querySelector('.btnErase');
+const btnYellow =document.querySelector('.btnYellow');
 
 function createGrid(size){
     // for(let u=0; u <= size ; u++){
-        for(let i = 0; i <= (size*size); i++ ){
+        for(let i = 0; i < (size*size); i++ ){
         const grid= document.createElement('div');
         container.appendChild(grid);
         grid.classList.add('grid');
@@ -11,3 +15,13 @@ function createGrid(size){
 }
 };
 createGrid(16);
+const square = document.querySelectorAll('.grid');
+btnBlack.addEventListener('click',hoverBlack());
+function hoverBlack(){
+    square.addEventListener('mouseover',
+    () => 
+    {e.target.style.backgroundColor='black'} 
+    )
+};
+btnErase.addEventListener('click', function (e){square.addEventListener('mouseover',function(e)  {square.style.backgroundColor='white'} );});
+btnYellow.addEventListener('click',function (e){square.addEventListener('mouseover',function(e)  {square.style.backgroundColor='yellow'} );});
