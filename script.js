89dae4730cb73959a5a309fe2cad181f5fe79540
yahyaeslem <yahyaeslem@gmail.com>
2022-12-17 14:12:30 +0300

    automated create grids

--- script.js
+++ script.js
@@ -1,48 +1,13 @@
-const container = document.querySelector('.container');
-const div1 = document.createElement('div');
-const div2 = document.createElement('div');
-const div3 = document.createElement('div');
-const div4 = document.createElement('div');
-const div5 = document.createElement('div');
-const div6 = document.createElement('div');
-const div7 = document.createElement('div');
-const div8 = document.createElement('div');
-const div9 = document.createElement('div');
-const div10 = document.createElement('div');
-const div11= document.createElement('div');
-const div12 = document.createElement('div');
-const div13 = document.createElement('div');
-const div14 = document.createElement('div');
-const div15 = document.createElement('div');
-const div16 = document.createElement('div');
-const div17 = document.createElement('div');
-const div18 = document.createElement('div');
-const div19 = document.createElement('div');
-const div20 = document.createElement('div');
-const div21 = document.createElement('div');
-div1.classList.add('.division');
-div2.classList.add('.division');
-div3.classList.add('.division');
-div4.classList.add('.division');
-div5.classList.add('.division');
-div6.classList.add('.division');
-div7.classList.add('.division');
-div8.classList.add('.division');
-div9.classList.add('.division');
-div10.classList.add('.division');
-div11.classList.add('.division');
-div12.classList.add('.division');
-div13.classList.add('.division');
-div14.classList.add('.division');
-div15.classList.add('.division');
-container.appendChild(div1);
-container.appendChild(div2);
-container.appendChild(div3);
-container.appendChild(div4);
-container.appendChild(div5);
-container.appendChild(div6);
-container.appendChild(div7);
-container.appendChild(div8);
-container.appendChild(div9);
-container.appendChild(div10);
+const container = document.querySelector('#container');
 
+
+function createGrid(size){
+    // for(let u=0; u <= size ; u++){
+        for(let i = 0; i <= (size*size); i++ ){
+        const grid= document.createElement('div');
+        container.appendChild(grid);
+        grid.classList.add('grid');
+       
+}
+};
+createGrid(16);
