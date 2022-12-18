afd3e3aa86cb7df568fd7a18483c76aa934c22d4
yahyaeslem <yahyaeslem@gmail.com>
2022-12-19 01:57:23 +0300

    final stage  - waiting for rainbow

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const container = document.querySelector('.container');
 const btnBlack =document.querySelector('.btnBlack');
 const btnErase =document.querySelector('.btnErase');
 const btnYellow =document.querySelector('.btnYellow');
+const reset = document.querySelector('.resetBtn');
 
 const gridClass =document.querySelectorAll('.grid');
 function createGrid(size){
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
 const gridSize= (720/size);
 for(let i = 0; i < (size*size); i++ ){
         const grid= document.createElement('div');
@@ -18,14 +20,20 @@ for(let i = 0; i < (size*size); i++ ){
        btnErase.addEventListener('click',
        ()=> {grid.addEventListener('mouseover' , ()=> {grid.style.backgroundColor ='white'})});
        btnYellow.addEventListener('click',
-       ()=> {grid.addEventListener('mouseover' , ()=> {grid.style.backgroundColor ='yellow'})});
+       ()=> {grid.addEventListener('mouseover' , ()=> {grid.style.backgroundColor = "#" + randomColor})});
        gridCounter  = size;
 }};
 
 createGrid(16);
- const smallBtn = document.querySelector('.smallBtn');
- smallBtn.addEventListener('click',()=>{
-        const size = prompt("Number");
-        container.innerHTML="";
-        createGrid(size);
-      });
+const smallBtn = document.querySelector('.smallBtn');
+const mediumBtn = document.querySelector('.mediumBtn');
+const biggerBtn = document.querySelector('.biggerBtn');
+//  smallBtn.addEventListener('click',()=>{
+//         const size = prompt("Number");
+//         container.innerHTML="";
+//         createGrid(size);
+//       });
+smallBtn.addEventListener('click',()=> {container.innerHTML=""; createGrid (48);});
+mediumBtn.addEventListener('click', ()=> {container.innerHTML=""; createGrid (24);});
+biggerBtn.addEventListener('click', ()=> {container.innerHTML=""; createGrid (8);});
+reset.addEventListener('click', ()=> {  container.innerHTML=""; createGrid(gridCounter);});
