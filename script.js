2feb6730f97799b0454f59413e802c7051313d0d
yahyaeslem <yahyaeslem@gmail.com>
2022-12-18 04:33:13 +0300

    prompt for size created

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
+let gridCounter;
 const container = document.querySelector('.container');
-
-
 const btnBlack =document.querySelector('.btnBlack');
 const btnErase =document.querySelector('.btnErase');
 const btnYellow =document.querySelector('.btnYellow');
-const grid= document.createElement('div');
-function createGrid(size){
 
+const gridClass =document.querySelectorAll('.grid');
+function createGrid(size){
+const gridSize= (720/size);
 for(let i = 0; i < (size*size); i++ ){
         const grid= document.createElement('div');
         container.appendChild(grid);
         grid.classList.add('grid');
-       btnBlack.addEventListener('click',()=> {grid.addEventListener('mouseover' , ()=> {grid.style.backgroundColor ='black'})});
-       btnErase.addEventListener('click',()=> {grid.addEventListener('mouseover' , ()=> {grid.style.backgroundColor ='white'})});
-       btnYellow.addEventListener('click',()=> {grid.addEventListener('mouseover' , ()=> {grid.style.backgroundColor ='yellow'})});
+         grid.style.width = gridSize+"px";
+         grid.style.height = gridSize + "px";
+       btnBlack.addEventListener('click',
+       ()=> {grid.addEventListener('mouseover' , ()=> {grid.style.backgroundColor ='black'})});
+       btnErase.addEventListener('click',
+       ()=> {grid.addEventListener('mouseover' , ()=> {grid.style.backgroundColor ='white'})});
+       btnYellow.addEventListener('click',
+       ()=> {grid.addEventListener('mouseover' , ()=> {grid.style.backgroundColor ='yellow'})});
+       gridCounter  = size;
 }};
-//  const square =document.querySelectorAll('.grid');
-//  btnBlack.addEventListener('click', ()=>{
-//     square.addEventListener('mouseover' , ()=> {square.style.backgroundColor ='black'})});
 
-// };
 createGrid(16);
-// const square = document.querySelectorAll('.grid');
-// btnBlack.addEventListener('click',hoverBlack());
-// function hoverBlack(){
-//     square.addEventListener('mouseover',
-// () => 
-//     {square.style.backgroundColor='black'}
-//     )
-// };
-// btnErase.addEventListener('click', function (e){square.addEventListener('mouseover',function(e)  {square.style.backgroundColor='white'} );});
-// btnYellow.addEventListener('click',function (e){square.addEventListener('mouseover',function(e)  {square.style.backgroundColor='yellow'} );});
+ const smallBtn = document.querySelector('.smallBtn');
+ smallBtn.addEventListener('click',()=>{
+        const size = prompt("Number");
+        container.innerHTML="";
+        createGrid(size);
+      });
